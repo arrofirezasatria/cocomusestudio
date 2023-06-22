c14ccfaece5d992b7fffcea9c6cec25944331474
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-06-22 12:59:20 +0700

    style: update

--- src/pages/simpleMap.tsx
+++ src/pages/simpleMap.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import GoogleMapReact from "google-map-react";
+// import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function SimpleMap() {
   const defaultProps = {
@@ -15,13 +15,13 @@ export default function SimpleMap() {
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
+      {/* <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyAYPAQvND3TRp3x2vxlr2bbVWfYkxw8bew" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-      </GoogleMapReact>
+      </GoogleMapReact> */}
     </div>
   );
 }
